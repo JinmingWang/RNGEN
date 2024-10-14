cc7eab0071498fdc258b9fbc8a81823617ae413d
JinmingWang <13106C1@gmail.com>
2024-10-14 22:22:01 +0100

    Double sequence hungarian loss

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,7 +1,6 @@
 from .HungarianLoss_SeqMat import HungarianLoss as HungarianLoss_SeqMat
 from .HungarianLoss_Sequential import HungarianLoss as HungarianLoss_Sequential
+from .HungarianLoss_DoubleSeq import HungarianLoss as HungarianLoss_DoubleSeq
 from .NodeEdgeModel import NodeEdgeModel
 from .SegmentsModel import SegmentsModel
-# from .TrajAutoEncoder import Encoder, Decoder
-# from .TrivialEncoder import Encoder
 from .TrajSplitEncoder import Encoder
