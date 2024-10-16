684ea5db30e81c7adc117f20540b633dbde4b666
JinmingWang <13106C1@gmail.com>
2024-10-16 13:03:09 +0100

    New Path Encoder

--- Models/PathEncoder.py
+++ Models/PathEncoder.py
@@ -1,26 +1,88 @@
+import torch
+
 from .Basics import *
 
-# This encoder splits the trajectories into sub-trajectories
-class Stage(nn.Sequential):
-    def __init__(self, N_trajs: int, L_traj: int, L_segment: int, D_token: int, downsample: bool = False):
-        self.N_trajs = N_trajs
-        self.L_traj = L_traj
-        self.L_segment = L_segment
-        self.D_token = D_token
-        super().__init__(
-            Rearrange("(B N) C L", "B N (L C)", N=N_trajs),
-            # The design: the idea is that each head will focus on a segment of the trajectory
-            AttentionBlock(d_in=L_traj * D_token, d_out=L_traj * D_token, d_head=L_segment * D_token,
-                           n_heads=L_traj // L_segment, d_expand=256),
-            AttentionBlock(d_in=L_traj * D_token, d_out=L_traj * D_token, d_head=L_segment * D_token,
-                           n_heads=L_traj // L_segment, d_expand=256),
 
-            Rearrange("B N (L C)", "(B N) C L", L=L_traj),
-            Res1D(D_token, D_token * 4, D_token),
-            Res1D(D_token, D_token * 4, D_token),
+class ConvSplitAttn(nn.Module):
+    def __init__(self, n_trajs: int, d_in: int, d_expand: int, d_out: int, n_splits: int, dropout: float=0.1):
+        super(ConvSplitAttn, self).__init__()
 
-            nn.Conv1d(D_token, D_token * (1 + int(downsample)), 3, 1 + int(downsample), 1),
+        # in shape: (BN, C, L)
+        # Q shape: (B*H, N, head_c)
+        # K shape: (B*H, N, head_c)
+        # V shape: (B*H, N, in_c)
+        self.N = n_trajs
+        self.S = n_splits
+        self.d_split = d_in // n_splits
+        self.d_in = d_in
+        self.dropout = nn.Dropout(dropout)
+
+        self.d_q = d_in
+        self.d_k = d_in
+        self.d_v = d_in
+        d_qkv = self.d_q + self.d_k + self.d_v
+
+        self.qkv_proj = nn.Sequential(
+            nn.GroupNorm(8, d_in),
+            nn.Conv1d(d_in, d_qkv, 3, 1, 1),
         )
+
+        self.merge_head_proj = nn.Conv1d(d_in, d_in, 3, 1, 1)
+
+        torch.nn.init.zeros_(self.merge_head_proj.weight)
+        torch.nn.init.zeros_(self.merge_head_proj.bias)
+
+        self.ff = nn.Sequential(
+            nn.GroupNorm(8, d_in),
+            nn.Conv1d(d_in, d_expand, 3, 1, 1),
+            Swish(),
+            nn.Dropout(dropout),
+            nn.Conv1d(d_expand, d_out, 3, 1, 1),
+        )
+
+        torch.nn.init.zeros_(self.ff[-1].weight)
+        torch.nn.init.zeros_(self.ff[-1].bias)
+
+        self.shortcut = nn.Conv1d(d_in, d_out, 1) if d_in != d_out else nn.Identity()
+
+    def forward(self, x):
+
+        qkv = rearrange(self.qkv_proj(x), '(B N) (C T) (L S) -> T B (N S) (C L)', N=self.N, T=3, S=self.S)
+        q, k, v = torch.unbind(qkv, dim=0)
+
+        # attn shape: (B*H, N, N)
+        attn = 1 / torch.cdist(q, k, p=2)    # why not use softmax? because one token can attend to multiple tokens
+        attn = self.dropout(attn)
+
+        # out shape: (B, N, H*in_c)
+        out = rearrange(torch.bmm(attn, v), 'B (N S) (C L) -> (B N) C (L S)', N=self.N, S=self.S, C=self.d_in)
+        x = x + self.merge_head_proj(out)
+
+        return self.ff(x) + self.shortcut(x)
+
+
+# This encoder splits the trajectories into sub-trajectories
+class Stage(nn.Module):
+    def __init__(self, N_trajs: int, D_token: int, downsample: bool = False):
+        super().__init__()
+        self.N_trajs = N_trajs
+        self.D_token = D_token
+
+        self.branches_1_1 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+        self.branches_1_2 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+        self.branches_1_3 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+
+        self.branches_2_1 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+        self.branches_2_2 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+        self.branches_2_3 = ConvSplitAttn(N_trajs, D_token, D_token * 4, D_token, 16)
+
+
+        self.out_proj = nn.Conv1d(D_token, D_token * (1 + int(downsample)), 3, 1 + int(downsample), 1)
+
+    def forward(self, x):
+        x = self.branches_1_1(x) + self.branches_1_2(x) + self.branches_1_3(x)
+        x = self.branches_2_1(x) + self.branches_2_2(x) + self.branches_2_3(x)
+        return self.out_proj(x)
 
 
 class PathEncoder(nn.Module):
@@ -35,22 +97,21 @@ class PathEncoder(nn.Module):
         self.s0 = nn.Sequential(
             Rearrange("B N L C", "(B N) C L"),
             Res1D(2, 64, 16),
-            Res1D(16, 64, 16),
-            nn.Conv1d(16, 16, 3, 2, 1),
+            Res1D(16, 64, 32),
             # (BN, C=8, L=32)
         )
-
         # (BN, C=8, L=64)
 
-        self.s1 = Stage(N_trajs, L_traj // 2, 4, 16, True)     # (BN, C=16, L=16)
+        self.s1 = Stage(N_trajs, 32, True)     # (BN, C=16, L=16)
 
-        self.s2 = Stage(N_trajs, L_traj // 4, 4, 32)     # (BN, C=16, L=16)
+        self.s2 = Stage(N_trajs, 64, True)     # (BN, C=16, L=16)
 
         self.head = nn.Sequential(
             Rearrange("(B N) C L", "B N (L C)", N=N_trajs),     # (B, N, LC=512)
-            AttentionBlock(d_in=L_traj * 8, d_out=256, d_head=64, n_heads=8, d_expand=256),
-            AttentionBlock(d_in=256, d_out=64, d_head=64, n_heads=8, d_expand=256),
-            AttentionBlock(d_in=64, d_out=L_path * 2, d_head=64, n_heads=8, d_expand=256),
+            nn.Linear(L_traj * 32, 512),
+            AttentionBlock(d_in=512, d_out=128, d_head=64, n_heads=8, d_expand=256),
+            AttentionBlock(d_in=128, d_out=32, d_head=64, n_heads=8, d_expand=256),
+            AttentionBlock(d_in=32, d_out=L_path * 2, d_head=64, n_heads=8, d_expand=256),
             nn.Linear(L_path * 2, L_path * 2),
             Rearrange("B N (L C)", "B N L C", L=L_path),
         )
