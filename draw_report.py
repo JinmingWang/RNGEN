6e6c47560f2ac7c12986e2ebe8e5646a72a65609
JinmingWang <13106C1@gmail.com>
2024-11-23 21:26:35 +0000

    Update Draw plot

--- draw_report.py
+++ draw_report.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-report = pd.read_csv("Report.csv")
+report = pd.read_csv("Report_all.csv")
 
 
 colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "cyan"]
@@ -18,14 +18,28 @@ model_names = report["name"].unique()
 
 for i, title in enumerate(titles):
     axes[i].set_title(title)
-    # violin plot
+    # draw grid
+    axes[i].grid(True)
+    axes[i].set_xlabel("Models")
+    axes[i].set_ylabel(title.replace("_", " "))
 
     model_data = [np.array(report[report["name"] == name][title].values) for name in model_names]
-    parts = axes[i].violinplot(model_data, showmeans=True, showmedians=True, showextrema=False)
+    parts = axes[i].violinplot(model_data, showmeans=False, showmedians=False, showextrema=False)
     for j, pc in enumerate(parts['bodies']):
         pc.set_facecolor(colors[j])
-        pc.set_edgecolor('black')
-        pc.set_alpha(0.5)
+        # pc.set_edgecolor('black')
+        pc.set_alpha(0.3)
+
+        quartile1, medians, quartile3 = np.percentile(model_data[j], [25, 50, 75])
+
+        # Draw a small line at median
+        axes[i].scatter(j+1, medians, color='black', s=100, zorder=3, marker="_")
+        axes[i].vlines(j+1, quartile1, quartile3, color='black', linestyle='-', lw=5)
+        axes[i].vlines(j+1, np.min(model_data[j]), np.max(model_data[j]), color='black', linestyle='-', lw=1)
+
+        # Set x-axis labels
+        axes[i].set_xticks(np.arange(1, len(model_names) + 1))
+        axes[i].set_xticklabels(model_names)
 
 plt.tight_layout()
 
